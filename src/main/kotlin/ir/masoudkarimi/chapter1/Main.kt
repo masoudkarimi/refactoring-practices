88d8e2dce562d7e8f097267b829db88755e1ea06
masoud <karimi.masoud5494@gmail.com>
2024-05-16 15:02:54 +0330

    chapter 1 - local functions

--- src/main/kotlin/ir/masoudkarimi/chapter1/Main.kt
+++ src/main/kotlin/ir/masoudkarimi/chapter1/Main.kt
@@ -13,76 +13,74 @@ fun main() {
 
 
 fun statement(invoice: Invoice, plays: Plays): String {
+    fun playFor(performance: Performance) = plays[performance.playId]!!
+
+    fun amountFor(performance: Performance): Int {
+        var result: Int
+
+        when (playFor(performance).type) {
+            "tragedy" -> {
+                result = 40000
+                if (performance.audience > 30) {
+                    result += 1000 * (performance.audience - 30)
+                }
+            }
+
+            "comedy" -> {
+                result = 30000
+                if (performance.audience > 20) {
+                    result += 10000 + 500 * (performance.audience - 20)
+                }
+                result += 300 * performance.audience
+            }
+
+
+            else -> {
+                throw Error("unknown type: ${playFor(performance).type}")
+            }
+        }
+
+        return result
+    }
+
+    fun totalAmount(): Int {
+        var result = 0
+        for (perf in invoice.performances) {
+            result += amountFor(perf)
+        }
+        return result
+    }
+
+    fun usd(number: Int): String {
+        return NumberFormat.getCurrencyInstance(Locale.US).apply {
+            minimumFractionDigits = 2
+        }.format(number / 100)
+    }
+
+    fun volumeCreditsFor(performance: Performance): Int {
+        var result = 0
+        result += max(performance.audience - 30, 0)
+        if (playFor(performance).type == "comedy") {
+            result += floor(performance.audience / 5.0).toInt()
+        }
+
+        return result
+    }
+
+    fun totalVolumeCredits(): Int {
+        var result = 0
+        for (perf in invoice.performances) {
+            result += volumeCreditsFor(perf)
+        }
+        return result
+    }
+
     var result = "Statement for ${invoice.customer}\n"
-
     for (perf in invoice.performances) {
-
         result += " ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n"
     }
 
-    result += "Amount owed is ${usd(totalAmount(invoice))}\n"
-    result += "You earned ${totalVolumeCredits(invoice)} credits"
+    result += "Amount owed is ${usd(totalAmount())}\n"
+    result += "You earned ${totalVolumeCredits()} credits"
     return result
 }
-
-private fun totalAmount(invoice: Invoice): Int {
-    var result = 0
-    for (perf in invoice.performances) {
-        result += amountFor(perf)
-    }
-    return result
-}
-
-private fun totalVolumeCredits(invoice: Invoice): Int {
-    var result = 0
-    for (perf in invoice.performances) {
-        result += volumeCreditsFor(perf)
-    }
-    return result
-}
-
-private fun usd(number: Int): String {
-    return NumberFormat.getCurrencyInstance(Locale.US).apply {
-        minimumFractionDigits = 2
-    }.format(number / 100)
-}
-
-private fun volumeCreditsFor(performance: Performance): Int {
-    var result = 0
-    result += max(performance.audience - 30, 0)
-    if (playFor(performance).type == "comedy") {
-        result += floor(performance.audience / 5.0).toInt()
-    }
-
-    return result
-}
-
-private fun amountFor(performance: Performance): Int {
-    var result: Int
-
-    when (playFor(performance).type) {
-        "tragedy" -> {
-            result = 40000
-            if (performance.audience > 30) {
-                result += 1000 * (performance.audience - 30)
-            }
-        }
-
-        "comedy" -> {
-            result = 30000
-            if (performance.audience > 20) {
-                result += 10000 + 500 * (performance.audience - 20)
-            }
-            result += 300 * performance.audience
-        }
-
-
-        else -> {
-            throw Error("unknown type: ${playFor(performance).type}")
-        }
-    }
-
-    return result
-}
-
-private fun playFor(performance: Performance) = plays[performance.playId]!!
